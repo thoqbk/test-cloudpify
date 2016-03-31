1d4b6cd9a3c9f783cc50eb100777f981256f91ea
Tho Q Luong <thoqbk@gmail.com>
2016-03-31 22:29:03 +0700

    Add a test for expressjs

---
+++ test/test-expressjs.js
@@ -0,0 +1,99 @@
+/**
+ * Copyright (C) 2016, Cloudpify
+ * 
+ * Tho Q Luong <thoqbk@gmail.com>
+ * 
+ * March 31, 2016
+ * 
+ */
+
+
+var expect = require("chai").expect;
+var assert = require("chai").assert;
+
+var Q = require("q");
+
+var $config = require("../config/app.js");
+
+var log4js = require("log4js");
+log4js.configure("./config/log4js.json");
+var $logger = log4js.getLogger("app");
+
+var userService = new (require("../service/sample-user-service"))();
+
+var authenticationService = new (require("../lib/service/authentication-service.js"))(function () {
+    return userService;
+}, $config, $logger);
+
+var httpClient = require($config.https.enable ? "https" : "http");
+
+describe("test expressjs request and response", function () {
+    it("should return default page with code 200 or 404", function (done) {
+        var cloudpifyAppHandler = new (require("../lib/handler.js"))();
+        cloudpifyAppHandler.start()
+                .then(function () {
+                    return sendHttpRequest("/", true);
+                })
+                .then(function (statusCode) {
+                    expect(statusCode).to.equal(200);
+                    return sendHttpRequest("/", false);
+                })
+                .then(function (statusCode) {
+                    expect(statusCode).to.equal(200);
+                    return sendHttpRequest("/random-url-" + Math.floor(Math.random() * 100000000), true);
+                })
+                .then(function (statusCode) {
+                    expect(statusCode).to.equal(404);
+                    return sendHttpRequest("/random-url-" + Math.floor(Math.random() * 100000000), false);
+                })
+                .then(function (statusCode) {
+                    expect(statusCode).to.equal(404);
+                    return cloudpifyAppHandler.stop();
+
+                })
+                .then(function () {
+                    done();
+                })
+                .fail(done);
+    });
+});
+
+
+function sendHttpRequest(path, isPost) {
+
+    var retVal = Q.defer();
+
+    var options = {
+        hostname: "localhost",
+        port: 5102,
+        path: path,
+        method: isPost ? "POST" : "GET",
+        headers: {
+            userId: 1,
+            'Content-Type': 'application/json'
+        }
+    };
+
+    if ($config.https.enable) {
+        options.requestCert = true;
+        options.rejectUnauthorized = false;
+    }
+
+    var request = httpClient.request(options, function (response) {
+        response.setEncoding("utf8");
+        var responseInString = "";
+        response.on("data", function (chunk) {
+            responseInString += chunk;
+        });
+        response.on("end", function () {
+            retVal.resolve(response.statusCode);
+        });
+    });
+    request.on("error", function (error) {
+        retVal.reject(error);
+    });
+
+    request.end();
+
+    return retVal.promise;
+}
